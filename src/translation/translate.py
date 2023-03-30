8cdd18889feb40d0f3ade9e2b9a9a399881ee0b1
Kei Koyanagi <81954389+KeiKoyanagi@users.noreply.github.com>
2023-03-30 19:23:27 +0900

    translated en into fr from 42 to 66

---
+++ src/translation/translate.py
@@ -0,0 +1,42 @@
+import requests
+import os
+
+API_KEY = 'ea4966da-5551-d78e-849f-8e1a50ab4187:fx'
+
+class translation():
+    def __init__(self, file):
+        self.file = file
+
+    def translate(self):
+        with open('../../data/en/{}'.format(self.file), 'r') as f:
+            self.source_file_l = f.read()
+            #print(self.source_file)
+        
+        url = 'https://api-free.deepl.com/v2/translate'
+        params = {
+            'auth_key': API_KEY,
+            'text': self.source_file_l,
+            'source_lang': 'EN',
+            'target_lang': 'FR'
+        }
+
+        request = requests.post(url, data=params)
+        result = request.json()
+        #print(result)
+        
+        with open('../../data/fr/{}'.format(self.file), 'w') as f:
+            f.write(result['translations'][0]['text'])
+        
+        
+        
+
+
+
+if __name__ == '__main__':
+    path = '../../data/en/'
+    files = os.listdir(path)
+    files = sorted(files)
+    for i in range(1, 100):
+        print(files[i])
+        t = translation(files[i])
+        t.translate()
