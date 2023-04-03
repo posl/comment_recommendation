50db064448b091bc3d121c942b389a76bf819caf
Kei Koyanagi <81954389+KeiKoyanagi@users.noreply.github.com>
2023-04-03 16:54:53 +0900

    translated English into Chinese

--- src/translation/translate.py
+++ src/translation/translate.py
@@ -14,19 +14,19 @@ class translation():
             self.source_file_l = f.read()
             #print(self.source_file)
         
-        url = 'https://api-free.deepl.com/v2/translate'
+        url = 'https://api.deepl.com/v2/translate'
         params = {
             'auth_key': API_KEY,
             'text': self.source_file_l,
             'source_lang': 'EN',
-            'target_lang': 'FR'
+            'target_lang': 'ZH'
         }
 
         request = requests.post(url, data=params)
         result = request.json()
         #print(result)
         
-        with open('../../data/fr/{}'.format(self.file), 'w') as f:
+        with open('../../data/zh/{}'.format(self.file), 'w') as f:
             f.write(result['translations'][0]['text'])
         
         
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     path = '../../data/en/'
     files = os.listdir(path)
     files = sorted(files)
-    for i in range(1, 100):
-        print(files[i])
-        t = translation(files[i])
+    for file in files:
+        print(file)
+        t = translation(file)
         t.translate()
