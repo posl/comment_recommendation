dc0307cc85154f6ea0e4bfc3aa490451ee1d0a18
Kei Koyanagi <81954389+KeiKoyanagi@users.noreply.github.com>
2023-05-31 19:31:37 +0900

    modified cog and made div accu

--- src/cal_cognitive/cal_cognitive.py
+++ src/cal_cognitive/cal_cognitive.py
@@ -5,26 +5,27 @@ import csv
 import shutil
 
 class cal_cognitive:
-    def __init__(self, base_path, script_dir_path, result_path):
-        self.base_path = base_path
-        self.script_dir_path = script_dir_path
+    def __init__(self, script_dir_base_path, target_base_each_l, result_path):
+        self.script_dir_path = script_dir_base_path
+        self.target_base_each_l = target_base_each_l
         self.result_path = result_path
 
     def main(self):
         all_result = []
         try:
-            script_l = sorted(os.listdir(self.base_path + self.script_dir_path))
-            for script in script_l:
+            problem_number = self.target_base_each_l[0].split(',')[0]
+            for script in self.target_base_each_l:
                 each_result = []
-                with open(self.base_path + self.script_dir_path + '/' + script, 'r') as f:
+                with open('{0}/{1}_{2}/{3}.py'.format(self.script_dir_path, script.split(',')[0], script.split(',')[1], script.split(',')[2]), 'r') as f:
                     tree = ast.parse(f.read())
                     result = get_cognitive_complexity(tree.body[0])
                     each_result.append(result)
-                    each_result.append(script)
+                    each_result.append(script.split(',')[1])
+                    each_result.append(script.split(',')[2])
                     all_result.append(each_result)
-            with open('{0}/{1}.csv'.format(self.result_path, self.script_dir_path.split('/')[-1]), 'w') as f:
+            with open('{0}/{1}.csv'.format(self.result_path, problem_number), 'w') as f:
                 writer = csv.writer(f)
-                writer.writerow(['cognitive_complexity', 'filename'])
+                writer.writerow(['cognitive_complexity', 'difficulty', 'filename'])
                 writer.writerows(all_result)
         except:
             print('error')
@@ -32,19 +33,27 @@ class cal_cognitive:
 if __name__ == '__main__':
     print('input times')
     times = input() + '_time'
+    minimum = float(1)
+    maximum = float(1)
     language_l = ['en', 'ja']
+    base_path = '/Users/keikoyanagi/Desktop/comment_recommendation'
 
     for language in language_l:
-        del_dir_path = '/Users/keikoyanagi/Desktop/comment_recommendation/result/cognitive/each/{0}/{1}/'.format(times, language)
+        del_dir_path = '{0}/result/cognitive/each/{1}/{2}/'.format(base_path, times, language)
         if os.path.exists(del_dir_path):
             shutil.rmtree(del_dir_path)
         os.mkdir(del_dir_path)
-        base_path = '/Users/keikoyanagi/Desktop/comment_recommendation/script/split_gen/{0}/{1}'.format(times, language)
-        result_path = '/Users/keikoyanagi/Desktop/comment_recommendation/result/cognitive/each/{0}/{1}'.format(times, language)
-        problem_l = sorted(os.listdir(base_path))
-        for problem in problem_l:
-            script_dir_path = '/{0}'.format(problem)
-            a = cal_cognitive(base_path, script_dir_path, result_path)
+        script_dir_base_path = '{0}/script/mod_gen/{1}/{2}'.format(base_path, times, language)
+        result_path = '{0}/result/cognitive/each/{1}/{2}'.format(base_path, times, language)
+        target_base_path = '{0}/result/target_complexity/{1}_{2}/{3}/{4}'.format(base_path, str(minimum), str(maximum), times, language)
+        #[099.txt, ~, 287.txt]
+        target_base_l = sorted(os.listdir(target_base_path))
+        for each_txt in target_base_l:
+            with open('{0}/{1}'.format(target_base_path, each_txt), 'r') as f:
+                #[[099,A,0], [099,A,1], ...]
+                target_base_each_l = f.readlines()
+                target_base_each_l = [each_txt.rstrip('\n') for each_txt in target_base_each_l]
+            a = cal_cognitive(script_dir_base_path, target_base_each_l, result_path)
             a.main()
-            print(problem, language)
+            print(each_txt.split('.')[0], language)
             break
---
+++ src/sum/div_accuracy.py
@@ -0,0 +1,47 @@
+import csv
+import os
+import pandas as pd
+
+class Div_accuracy:
+    def __init__(self, base_path, times, language):
+        self.base_path = base_path
+        self.result_path = base_path + '/result/accuracy/all/{0}/{1}/'.format(times, language)
+        self.times = times
+        self.language = language
+        self.each_result_l = sorted(os.listdir('{0}/result/accuracy/each/{1}/{2}/'.format(self.base_path, self.times, self.language)))
+        
+
+    def div_accuracy(self):
+        div_accuracy_l = [[0 for i in range(11)], [0 for i in range(11)], [0 for i in range(11)], [0 for i in range(11)]]
+        for each_result in self.each_result_l:
+            main_df = pd.read_csv('{0}/result/accuracy/each/{1}/{2}/{3}'.format(self.base_path, self.times, self.language, each_result))
+
+            for i, difficulty in enumerate(['A', 'B', 'C', 'D']):
+                df = main_df[main_df['difficulty'] == difficulty]
+                df = df['accuracy']
+                div = pd.cut(df,  bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6,0.7, 0.8, 0.9, 1.0, 1.1], right=False)
+                div = div.value_counts(sort=False).tolist()
+                for j in range(len(div)):
+                    div_accuracy_l[i][j] += div[j]
+        return div_accuracy_l
+        
+    
+    def write(self, div_accuracy_l):
+        with open('{0}/div_accuracy.csv'.format(self.result_path), 'w') as f:
+            writer = csv.writer(f)
+            percentage_l = []
+            for percentage in range(0, 110, 10):
+                percentage_l.append(str(percentage) + '%')
+            writer.writerow(['difficulty'] + percentage_l)
+            for i, difficulty in enumerate(['A', 'B', 'C', 'D']):
+                writer.writerow([difficulty] + div_accuracy_l[i])
+
+if __name__ == '__main__':
+    base_path = '/Users/keikoyanagi/Desktop/comment_recommendation'
+    print('input times')
+    times = input() + '_time'
+    print('input language')
+    language = input()
+    div_accuracy = Div_accuracy(base_path, times, language)
+    div_accuracy_l = div_accuracy.div_accuracy()
+    div_accuracy.write(div_accuracy_l)
