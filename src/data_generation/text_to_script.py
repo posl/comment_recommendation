d69df4a2f5ce05a64d3bb8136ff67ac3854dde22
Kei Koyanagi <81954389+KeiKoyanagi@users.noreply.github.com>
2023-05-31 19:55:15 +0900

    modified text_to_script

--- src/data_generation/text_to_script.py
+++ src/data_generation/text_to_script.py
@@ -2,14 +2,14 @@ import os
 import re
 
 if __name__ == '__main__':
-    base_path = '../../data/en/'
-    result_path = '../../script/pre_gen/en'
-    files_l = os.listdir(base_path)
+    path = '../../data/zh_checked/'
+    files_l = os.listdir(path)
     files_l = sorted(files_l)
+    files_l.remove('.DS_Store')
 
-    for language in ['en', 'ja','zh']:
+    for language in ['en', 'ja','zh_checked']:
         base_path = '../../data/{}/'.format(language)
-        result_path = '../../script/pre_gen/{}/'.format(language)
+        result_path = '../../script/pre_gen/{}/'.format(language.split('_')[0])
         for file in files_l:
             with open(base_path + file, 'r') as f:
                 source_file_l = f.readlines()
