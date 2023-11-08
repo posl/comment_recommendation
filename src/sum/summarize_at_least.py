de1ae16c1e77ed29ce694da166c6451f9818ea25
Kei Koyanagi <81954389+KeiKoyanagi@users.noreply.github.com>
2023-11-09 08:01:45 +0900

    made tables

--- src/sum/summarize_at_least.py
+++ src/sum/summarize_at_least.py
@@ -11,15 +11,33 @@ class Summarize_at_least:
     def permutate_at_least(self):
         df = pd.read_csv('{0}/{1}_at_least.csv'.format(self.each_result, self.language))
         df = df.sort_values(['difficulty', 'times'], ascending=True, ignore_index=True)
-        df.to_csv('{0}/{1}_at_least.csv'.format(self.each_result, self.language), index=False)
+        df.to_csv('{0}/per_{1}_at_least.csv'.format(self.each_result, self.language), index=False)
     
     def get_median(self):
-        df = pd.read_csv('{0}/{1}_at_least.csv'.format(self.each_result, self.language))
+        df = pd.read_csv('{0}/per_{1}_at_least.csv'.format(self.each_result, self.language))
         df = df.sort_values(['difficulty', 'times'], ascending=True, ignore_index=True).drop(['times', 'pass_num','total_num'], axis=1)
         df = df.groupby(['difficulty']).median().T
         df = df.rename(index={'accuracy': self.language})
         return df
         #df.to_csv('{0}/{1}_at_least_median.csv'.format(self.each_result, self.language), index=False)
+        
+    def make_table(self):
+        df = pd.read_csv('{0}/per_{1}_at_least.csv'.format(self.each_result, self.language))
+        df = df.reindex(columns=['times', 'difficulty', 'accuracy', 'pass_num', 'total_num'])
+        df.drop(['difficulty'], axis=1, inplace=True)
+        for i in range(len(df)):
+            df.iloc[i, 1] = df.iloc[i, 1].round(3)
+        df['proportion'] = df['pass_num'].astype(str) + ' / ' + df['total_num'].astype(str)
+        df.drop(['pass_num', 'total_num'], axis=1, inplace=True)
+        for i in range(4):
+            each_df = df.iloc[i*5:(i+1)*5, :]
+            each_df = each_df.reset_index(drop=True)
+            if i == 0:
+                all_df = each_df
+            else:
+                all_df = pd.concat([all_df, each_df.drop(['times'], axis = 1)], axis=1)
+        
+        all_df.to_csv('{0}/table_{1}_at_least.csv'.format(self.each_result, self.language), index=False)
 
 if __name__ == '__main__':
     base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -34,5 +52,6 @@ if __name__ == '__main__':
         summarize_at_least = Summarize_at_least(base_path, language)
         summarize_at_least.permutate_at_least()
         each_df = summarize_at_least.get_median()
+        df = summarize_at_least.make_table()
         all_df = each_df if language == language_l[0] else pd.concat([all_df, each_df], axis=0)
     all_df.to_csv('{0}/result/accuracy/all/at_least/at_least_median.csv'.format(base_path), index=True)
