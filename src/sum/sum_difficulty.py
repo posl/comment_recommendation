5f087e79a10c6d39e7bc648f9746b2f8305a1eba
Kei Koyanagi <81954389+KeiKoyanagi@users.noreply.github.com>
2024-04-13 17:40:30 +0900

    added/

---
+++ src/sum/sum_difficulty.py
@@ -0,0 +1,49 @@
+import csv
+import os
+import pandas as pd
+
+class Sum_difficulty:
+    def __init__(self, language):
+        self.base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        self.result_path = '{0}/result/accuracy/all/top/{1}'.format(self.base_path, language)
+        self.read_path = '{0}/result/accuracy/top'.format(self.base_path)
+        self.language = language
+
+    def main(self):
+        main_df = self.sum_accuracy()
+        self.write(main_df)
+
+    def sum_accuracy(self):
+        a_df = pd.DataFrame()
+        b_df = pd.DataFrame()
+        c_df = pd.DataFrame()
+        d_df = pd.DataFrame()
+        for i in range(1, 6):
+            each_df = pd.read_csv('{0}/{1}_time/{2}/sum_accuracy.csv'.format(self.read_path, str(i), self.language))
+            for difficulty in ['A', 'B', 'C', 'D']:
+                df = each_df[each_df['difficulty'] == difficulty]
+                if difficulty == 'A':
+                    a_df = pd.concat([a_df, df])
+                elif difficulty == 'B':
+                    b_df = pd.concat([b_df, df])
+                elif difficulty == 'C':
+                    c_df = pd.concat([c_df, df])
+                elif difficulty == 'D':
+                    d_df = pd.concat([d_df, df])
+        main_df = pd.concat([a_df, b_df, c_df, d_df])
+        return main_df
+    
+    def write(self, df):
+        os.makedirs(self.result_path, exist_ok=True)
+        df.to_csv('{0}/sum_all_accuracy.csv'.format(self.result_path), index=False)
+
+if __name__ == '__main__':
+    language_l = []
+    while True:
+        language = input('Enter language: en, ja, zh, or press enter to exit:')
+        if language == '':
+            break
+        language_l.append(language)
+    for language in language_l:
+        sum_difficulty = Sum_difficulty(language)
+        sum_difficulty.main()
